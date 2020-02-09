147bd85c4e6c85e6b0392063aaa6c5cace3b8e8b
Nathan Owens <ndowens@artixlinux.org>
2020-02-08 19:50:38 -0600

    update weather

--- scripts/get_weather.sh
+++ scripts/get_weather.sh
@@ -3,7 +3,7 @@
 
 while [ 1 ]
 do
-    weather=$(curl -Ss 'https://wttr.in/Pontevedra?0&T&Q&format=1')
+    weather=$(curl -Ss 'https://wttr.in/Geraldine?0&T&Q&format=1')
     echo "$weather"
     sleep 60
 done
--- scripts/sway_bar.sh
+++ scripts/sway_bar.sh
@@ -15,8 +15,8 @@ current_time=$(date "+%H:%M")
 #############
 
 # Battery or charger
-battery_charge=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "percentage" | awk '{print $2}')
-battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state" | awk '{print $2}')
+#battery_charge=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "percentage" | awk '{print $2}')
+#battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state" | awk '{print $2}')
 
 # Audio and multimedia
 audio_volume=$(pamixer --sink `pactl list sinks short | grep RUNNING | awk '{print $1}'` --get-volume)
@@ -28,7 +28,7 @@ player_status=$(playerctl status)
 # Network
 network=$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
 # interface_easyname grabs the "old" interface name before systemd renamed it
-interface_easyname=$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
+interface_easyname=eno1 #$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
 ping=$(ping -c 1 www.google.es | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
 
 # Others
@@ -37,7 +37,7 @@ loadavg_5min=$(cat /proc/loadavg | awk -F ' ' '{print $2}')
 
 # Removed weather because we are requesting it too many times to have a proper
 # refresh on the bar
-#weather=$(curl -Ss 'https://wttr.in/Pontevedra?0&T&Q&format=1')
+#weather=$(curl -Ss 'https://wttr.in/Geraldine?0&T&Q&format=1')
 
 if [ $battery_status = "discharging" ];
 then
